a409e05a5cdc83b933b10218ea19c6e7183a0f5f
arsenicer <574081@qq.com>
2017-12-13 20:58:32 +0800

    create project

---
+++ src/main/java/cn/edu/zjweu/cs/softconstruct/web/servlet/LoginServlet.java
@@ -0,0 +1,20 @@
+package cn.edu.zjweu.cs.softconstruct.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet",urlPatterns = "/loginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("UserName","jack");
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhangliang
+  Date: 2017/12/5
+  Time: 17:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  hello, Mr. ${UserName},now is <%=new java.util.Date()%>
+  </body>
+</html>
